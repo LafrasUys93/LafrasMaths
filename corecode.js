94e0a36656a5ff393d1b9b6c67f5ebf1c3ff0a21
Theuns <thns.pretorius@gmail.com>
2024-10-05 11:39:28 +0200

    Update corecode.js

--- corecode.js
+++ corecode.js
@@ -58,7 +58,7 @@ function chooseLanguage(language){
     });
     if (language == "Afrikaans"){
         // Title
-        titletext.textContent = "Lafras Wiskunde"
+        titletext.textContent = "Mnr Uys Wiskunde"
 
         // Main Page Visibility
         mainPageAfrikaans.style = "display: block"
@@ -67,7 +67,7 @@ function chooseLanguage(language){
     }
     else if (language == "English"){
         // Title
-        titletext.textContent = "Lafras Maths"
+        titletext.textContent = "Mr Uys Maths"
 
         // Main Page visibility
         mainPageAfrikaans.style = "display: none"
@@ -85,6 +85,13 @@ function main_page_english_div(){
     const gr8_link = document.createElement("a");
     gr8_link.id = "grade8LinkID";
     gr8_link.textContent ="Grade 8";
+    gr8_link.onclick = function(){
+        const gr8_page = document.getElementById("grade8DivID");
+        const language = document.getElementById("languageDivID");
+        gr8_page.style = "display: block;";
+        main_div.style = "display: none;";
+        language.style = "display: none;"
+    }
     main_div.appendChild(gr8_link);
 
     const br1 = document.createElement("br")
@@ -113,13 +120,74 @@ function main_page_afrikaans_div(){
     const gr10_link = document.createElement("a");
     gr10_link.id = "graad10LinkID";
     gr10_link.textContent ="Graad 10";
-    main_div.appendChild(gr10_link);}
+    main_div.appendChild(gr10_link);
+}
+
+function grade_8_div(){
+    const mainpage = document.getElementById("mainEnglishDivID")
+    const language = document.getElementById("languageDivID")
+    const grade_8_div = document.createElement("div");
+    grade_8_div.id = "grade8DivID";
+    grade_8_div.style = "display: none";
+
+    // Lesson Plans
+    const lesson_plans = document.createElement("a");
+    lesson_plans.id = "grade8LessonPlansID";
+    lesson_plans.textContent = "Lesson Plans";
+    grade_8_div.appendChild(lesson_plans);
+
+    const br1 = document.createElement("br")
+    grade_8_div.appendChild(br1)
+
+    // Worksheets
+    const worksheets = document.createElement("a");
+    worksheets.id = "grade8WorksheetsID";
+    worksheets.textContent = "Worksheets";
+    grade_8_div.appendChild(worksheets);
+
+    const br2 = document.createElement("br")
+    grade_8_div.appendChild(br2);
+
+    // Summaries
+    const summaries = document.createElement("a");
+    summaries.id = "grade8SummariesID";
+    summaries.textContent = "Summaries";
+    grade_8_div.append(summaries);
+
+    const br3 = document.createElement("br")
+    grade_8_div.appendChild(br3);
+
+    // Test Papers
+    const test_papers = document.createElement("a");
+    test_papers.id = "grade8TestPapersID";
+    test_papers.textContent = "Test Papers";
+    grade_8_div.appendChild(test_papers);
+
+    const br4 = document.createElement("br");
+    grade_8_div.appendChild(br4);
+    const br5 = document.createElement("br");
+    grade_8_div.appendChild(br5);
+
+    // Back Button
+    const back = document.createElement("a");
+    back.id = "grade8BackID";
+    back.textContent = "Back";
+    back.onclick = function(){
+        grade_8_div.style = "display: none";
+        language.style = "display: block";
+        mainpage.style = "display: block";
+    };
+    grade_8_div.appendChild(back)
+
+    body.appendChild(grade_8_div)
+}
 
 function main(){
     header_div();
     language_div();
     main_page_english_div();
     main_page_afrikaans_div();
+    grade_8_div()
 }
 
 main()
