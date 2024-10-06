865e7bd2cde882f648e33d27fd64616872615ff8
Theuns <thns.pretorius@gmail.com>
2024-10-06 11:12:09 +0200

    update

--- corecode.js
+++ corecode.js
@@ -72,13 +72,16 @@ let files = {   "Afrikaans" : {
 
 function header_div(){
     const sitenameheader_div = document.createElement("div");
+    sitenameheader_div.style = ""
     sitenameheader_div.id = "siteNameHeaderDivID";
     body.appendChild(sitenameheader_div)
 
     const sitenameheader_title = document.createElement("h1");
     sitenameheader_title.id = "siteNameHeaderTitleID";
-    sitenameheader_title.textContent = "Lafras Maths";
+    sitenameheader_title.textContent = "Mr Uys Maths";
+    const line = document.createElement("hr")
     sitenameheader_div.appendChild(sitenameheader_title);
+    sitenameheader_div.appendChild(line)
     
 }
 
@@ -118,8 +121,10 @@ function language_div(){
     language_form.appendChild(english_label);
     language_form.appendChild(afrikaans_label);
 
-    const br = document.createElement("br");
-    language_div.appendChild(br)
+    //const br = document.createElement("br");
+    const line = document.createElement("hr")
+    //language_div.appendChild(br)
+    language_div.appendChild(line)
 }
 
 function chooseLanguage(language){
@@ -158,7 +163,7 @@ function main_page_english_div(){
     main_div.style = "display: block;";
     body.appendChild(main_div);
 
-    const gr8_link = document.createElement("a");
+    const gr8_link = document.createElement("button");
     gr8_link.id = "grade8LinkID";
     gr8_link.textContent ="Grade 8";
     gr8_link.onclick = function(){
@@ -173,7 +178,7 @@ function main_page_english_div(){
     const br1 = document.createElement("br")
     main_div.appendChild(br1)
 
-    const gr10_link = document.createElement("a");
+    const gr10_link = document.createElement("button");
     gr10_link.id = "grade10LinkID";
     gr10_link.textContent ="Grade 10";
     gr10_link.onclick = function(){
@@ -192,7 +197,7 @@ function main_page_afrikaans_div(){
     main_div.style = "display: none;";
     body.appendChild(main_div);
 
-    const gr8_link = document.createElement("a");
+    const gr8_link = document.createElement("button");
     gr8_link.id = "graad8LinkID";
     gr8_link.textContent ="Graad 8";
     gr8_link.onclick = function(){
@@ -207,7 +212,7 @@ function main_page_afrikaans_div(){
     const br1 = document.createElement("br")
     main_div.appendChild(br1)
 
-    const gr10_link = document.createElement("a");
+    const gr10_link = document.createElement("button");
     gr10_link.id = "graad10LinkID";
     gr10_link.textContent ="Graad 10";
     gr10_link.onclick = function(){
@@ -232,7 +237,7 @@ function grade_8_div(){
     grade_8_div.style = "display: none";
 
     // Lesson Plans
-    const lesson_plans = document.createElement("a");
+    const lesson_plans = document.createElement("button");
     lesson_plans.id = "grade8LessonPlansID";
     lesson_plans.textContent = "Lesson Plans";
     grade_8_div.appendChild(lesson_plans);
@@ -241,7 +246,7 @@ function grade_8_div(){
     grade_8_div.appendChild(br1)
 
     // Worksheets
-    const worksheets = document.createElement("a");
+    const worksheets = document.createElement("button");
     worksheets.id = "grade8WorksheetsID";
     worksheets.textContent = "Worksheets";
     worksheets.onclick = function() {
@@ -254,7 +259,7 @@ function grade_8_div(){
     grade_8_div.appendChild(br2);
 
     // Summaries
-    const summaries = document.createElement("a");
+    const summaries = document.createElement("button");
     summaries.id = "grade8SummariesID";
     summaries.textContent = "Summaries";
     summaries.onclick = function(){
@@ -267,7 +272,7 @@ function grade_8_div(){
     grade_8_div.appendChild(br3);
 
     // Test Papers
-    const test_papers = document.createElement("a");
+    const test_papers = document.createElement("button");
     test_papers.id = "grade8TestPapersID";
     test_papers.textContent = "Test Papers";
     grade_8_div.appendChild(test_papers);
@@ -276,7 +281,7 @@ function grade_8_div(){
     grade_8_div.appendChild(br4);
 
     // Videos
-    const videos = document.createElement("a");
+    const videos = document.createElement("button");
     videos.id = "grade8Videos";
     videos.textContent = "Videos"
     grade_8_div.appendChild(videos)
@@ -287,7 +292,7 @@ function grade_8_div(){
     grade_8_div.appendChild(br6);
 
     // Back Button
-    const back = document.createElement("a");
+    const back = document.createElement("button");
     back.id = "grade8BackID";
     back.textContent = "Back";
     back.onclick = function(){
@@ -320,7 +325,7 @@ function grade_8_summaries_div(){
     const br2 = document.createElement("br");
     div.appendChild(br2)
 
-    const back = document.createElement("a")
+    const back = document.createElement("button")
     back.textContent = "Back"
     back.onclick = function() {
         div.style = "display: none"
@@ -350,7 +355,7 @@ function grade_8_worksheets_div(){
     const br2 = document.createElement("br");
     div.appendChild(br2)
 
-    const back = document.createElement("a")
+    const back = document.createElement("button")
     back.textContent = "Back"
     back.onclick = function() {
         div.style = "display: none"
@@ -368,7 +373,7 @@ function grade_10_div(){
     grade_10_div.style = "display: none";
 
     // Lesson Plans
-    const lesson_plans = document.createElement("a");
+    const lesson_plans = document.createElement("button");
     lesson_plans.id = "grade10LessonPlansID";
     lesson_plans.textContent = "Lesson Plans";
     grade_10_div.appendChild(lesson_plans);
@@ -377,7 +382,7 @@ function grade_10_div(){
     grade_10_div.appendChild(br1)
 
     // Worksheets
-    const worksheets = document.createElement("a");
+    const worksheets = document.createElement("button");
     worksheets.id = "grade8WorksheetsID";
     worksheets.textContent = "Worksheets";
     grade_10_div.appendChild(worksheets);
@@ -386,7 +391,7 @@ function grade_10_div(){
     grade_10_div.appendChild(br2);
 
     // Summaries
-    const summaries = document.createElement("a");
+    const summaries = document.createElement("button");
     summaries.id = "grade10SummariesID";
     summaries.textContent = "Summaries";
     grade_10_div.append(summaries);
@@ -395,7 +400,7 @@ function grade_10_div(){
     grade_10_div.appendChild(br3);
 
     // Test Papers
-    const test_papers = document.createElement("a");
+    const test_papers = document.createElement("button");
     test_papers.id = "grade10TestPapersID";
     test_papers.textContent = "Test Papers";
     grade_10_div.appendChild(test_papers);
@@ -404,7 +409,7 @@ function grade_10_div(){
     grade_10_div.appendChild(br4);
     
     // Videos
-    const videos = document.createElement("a");
+    const videos = document.createElement("button");
     videos.id = "grade10Videos";
     videos.textContent = "Videos"
     grade_10_div.appendChild(videos)
@@ -415,7 +420,7 @@ function grade_10_div(){
     grade_10_div.appendChild(br6);
 
     // Back Button
-    const back = document.createElement("a");
+    const back = document.createElement("button");
     back.id = "grade10BackID";
     back.textContent = "Back";
     back.onclick = function(){
@@ -442,7 +447,7 @@ function graad_8_div(){
     graad_8_div.style = "display: none";
 
     // Les Planne
-    const lesson_plans = document.createElement("a");
+    const lesson_plans = document.createElement("button");
     lesson_plans.id = "graad8LesPlanneID";
     lesson_plans.textContent = "Les Planne";
     graad_8_div.appendChild(lesson_plans);
@@ -451,7 +456,7 @@ function graad_8_div(){
     graad_8_div.appendChild(br1)
 
     // Werkkaarte
-    const werkkaarte = document.createElement("a");
+    const werkkaarte = document.createElement("button");
     werkkaarte.id = "werkKaarteID";
     werkkaarte.textContent = "Werkkaarte";
     werkkaarte.onclick = function() {
@@ -464,7 +469,7 @@ function graad_8_div(){
     graad_8_div.appendChild(br2);
 
     // Opsommings
-    const summaries = document.createElement("a");
+    const summaries = document.createElement("button");
     summaries.id = "graad8OpsommingsID";
     summaries.textContent = "Opsommings";
     summaries.onclick = function(){
@@ -477,7 +482,7 @@ function graad_8_div(){
     graad_8_div.appendChild(br3);
 
     // Toetse
-    const test_papers = document.createElement("a");
+    const test_papers = document.createElement("button");
     test_papers.id = "graad8ToetseID";
     test_papers.textContent = "Toetse";
     graad_8_div.appendChild(test_papers);
@@ -486,7 +491,7 @@ function graad_8_div(){
     graad_8_div.appendChild(br4);
     
     // Videos
-    const videos = document.createElement("a");
+    const videos = document.createElement("button");
     videos.id = "graad8Videos";
     videos.textContent = "Videos"
     graad_8_div.appendChild(videos)
@@ -497,7 +502,7 @@ function graad_8_div(){
     graad_8_div.appendChild(br6);
 
     // Terug Button
-    const back = document.createElement("a");
+    const back = document.createElement("button");
     back.id = "graad8TerugID";
     back.textContent = "Terug";
     back.onclick = function(){
@@ -530,7 +535,7 @@ function graad_8_opsommings_div(){
     const br2 = document.createElement("br");
     div.appendChild(br2)
 
-    const back = document.createElement("a")
+    const back = document.createElement("button")
     back.textContent = "Terug"
     back.onclick = function() {
         div.style = "display: none"
@@ -552,7 +557,7 @@ function graad_8_werkkaarte_div(){
         }
         const a = document.createElement("a")
         a.textContent = files["Afrikaans"]["Graad8"]["Werkkaarte"][i]
-        a.href = "Afrikaans/Graad8/Opsommings/" + files["Afrikaans"]["Graad8"]["Werkkaarte"][i]
+        a.href = "Afrikaans/Graad8/Werkkaarte/" + files["Afrikaans"]["Graad8"]["Werkkaarte"][i]
         div.appendChild(a)
     }
     const br = document.createElement("br");
@@ -560,7 +565,7 @@ function graad_8_werkkaarte_div(){
     const br2 = document.createElement("br");
     div.appendChild(br2)
 
-    const back = document.createElement("a")
+    const back = document.createElement("button")
     back.textContent = "Terug"
     back.onclick = function() {
         div.style = "display: none"
@@ -578,7 +583,7 @@ function graad_10_div(){
     graad_10_div.style = "display: none";
 
     // Les Planne
-    const lesson_plans = document.createElement("a");
+    const lesson_plans = document.createElement("button");
     lesson_plans.id = "graad10LessonPlansID";
     lesson_plans.textContent = "Les Planne";
     graad_10_div.appendChild(lesson_plans);
@@ -587,7 +592,7 @@ function graad_10_div(){
     graad_10_div.appendChild(br1)
 
     // Werkkaarte
-    const worksheets = document.createElement("a");
+    const worksheets = document.createElement("button");
     worksheets.id = "graad8WorksheetsID";
     worksheets.textContent = "Werkkaarte";
     graad_10_div.appendChild(worksheets);
@@ -596,7 +601,7 @@ function graad_10_div(){
     graad_10_div.appendChild(br2);
 
     // Opsommings
-    const summaries = document.createElement("a");
+    const summaries = document.createElement("button");
     summaries.id = "graad10SummariesID";
     summaries.textContent = "Opsommings";
     graad_10_div.append(summaries);
@@ -605,7 +610,7 @@ function graad_10_div(){
     graad_10_div.appendChild(br3);
 
     // Toetse
-    const test_papers = document.createElement("a");
+    const test_papers = document.createElement("button");
     test_papers.id = "graad10TestPapersID";
     test_papers.textContent = "Toetse";
     graad_10_div.appendChild(test_papers);
@@ -614,7 +619,7 @@ function graad_10_div(){
     graad_10_div.appendChild(br4);
     
     // Videos
-    const videos = document.createElement("a");
+    const videos = document.createElement("button");
     videos.id = "graad10Videos";
     videos.textContent = "Videos"
     graad_10_div.appendChild(videos)
@@ -625,7 +630,7 @@ function graad_10_div(){
     graad_10_div.appendChild(br6);
 
     // Terug Button
-    const back = document.createElement("a");
+    const back = document.createElement("button");
     back.id = "graad10BackID";
     back.textContent = "Terug";
     back.onclick = function(){
