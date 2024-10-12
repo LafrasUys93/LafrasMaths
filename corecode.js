496aa1378764fa834f1836ae8eda0c65b62384f7
Theuns <thns.pretorius@gmail.com>
2024-10-12 12:47:28 +0200

    visual updates

--- corecode.js
+++ corecode.js
@@ -1,5 +1,6 @@
 const body = document.body;
 var lang = "English"
+const background_clr = "#030325"
 
 const jsonString = '{"Afrikaans": {"Graad 08": {"Les planne": {"Kwartaal 1": 0, "Kwartaal 2": 0, "Kwartaal 3": 0, "Kwartaal 4": ["Lesplan Gr 8 Wiskunde Grafieke.pdf", "Lesplan Gr. 8 Wiskunde   Omtrek en Oppervlakte.pdf", "Lesplan Gr. 8 Wiskunde  Pythagoras.pdf"]}, "Opsommings": {"Kwartaal 1": 0, "Kwartaal 2": 0, "Kwartaal 3": ["1728275649807.jpg", "IMG_20241007_172225.jpg"], "Kwartaal 4": ["Gr 8 - Kwartaal 1&2 Wiskunde.pdf", "Gr 8 Algebra (1).pdf", "Gr 8 Algebra Bundle Kw 3 (1).pdf", "Gr 8 Breuke Fractions (1) 8.1.5.pdf", "Gr 8 Breuke Fractions (1) 8.1.5b.pdf", "Gr 8 Breuke Fractions (2) 8.1.6 (1).pdf", "Gr 8 Breuke Fractions (2) 8.1.6b.pdf", "Gr 8 Breuke Fractions (3) 8.1.7.pdf", "Gr 8 Breuke Fractions (4) 8.1.8.pdf", "Gr 8 Breuke Fractions (5) 8.1.9.pdf", "Gr 8 Eksponente exponents (1) 8.2.1.pdf", "Gr 8 Eksponente exponents (2) 8.2.2.pdf", "Gr 8 Eksponente exponents (3) 8.2.3.pdf", "Gr 8 Eksponente Exponents (4) 8.2.4.pdf", "Gr 8 Eksponente Exponents (5) 8.2.5.pdf", "Gr 8 Finansies Finance (1) 8.1.3 (1).pdf", "Gr 8 Finansies Finance (2) 8.1.4.pdf", "Gr 8 Getalpatrone Nr patterns (1) 8.2.6.pdf", "Gr 8 Koers Rate 8.1.3b.pdf", "Gr 8 Meetkunde 1 2024-07-19 12_58_23 (2).pdf", "Gr 8 Meetkunde 2 Copy (1).pdf", "Gr 8 Omtr Opp Per Area (2)8.3.8 (1).pdf", "Gr 8 Pythagoras 8.3.9 (1).pdf", "Gr 8 Telgetalle Whole nr (2) 8.1.2.pdf", "Gr 8 Telgetalle Whole nrs (2) 8.1.2.pdf", "Gr 8 Verhoudings ratios 8.1.3.pdf", "Gr 8.1.1 Telgetalle Whole numbers (1) (1).pdf", "Gr 8Algebra (2) 8.3.2.pdf", "Note 14 Aug 2023.pdf", "Stelling van Pythagoras.pdf", "Untitled spreadsheet.xlsx"]}, "Toets Papiere": {"Kwartaal 1": 0, "Kwartaal 2": 0, "Kwartaal 3": 0, "Kwartaal 4": 0}, "Werkkaarte": {"Kwartaal 1": ["Gr 8 - Kwartaal 1&2 Wiskunde.pdf"], "Kwartaal 2": ["Gr 8 - Kwartaal 1&2 Wiskunde.pdf"], "Kwartaal 3": ["Gr 8 - Kwartaal 3 - Wiskunde.pdf"], "Kwartaal 4": ["Gr 8 - Kwartaal 4 - Wiskunde hersiening.pdf"]}}, "Graad 10": 0}, "English": {"Grade 08": {"Lesson plans": {"Term 1": 0, "Term 2": 0, "Term 3": 0, "Term 4": ["Lesson Plan Gr 8 Mathematics Graphs.pdf", "Maths Gr  PYTHAGORAS.pdf", "Maths Gr.8  Perimeter and Area of 2 D Shapes.pdf"]}, "Summaries": {"Term 1": ["Gr 8 Breuke Fractions (1) 8.1.5.pdf", "Gr 8 Breuke Fractions (1) 8.1.5b.pdf", "Gr 8 Breuke Fractions (2) 8.1.6 (1).pdf", "Gr 8 Breuke Fractions (2) 8.1.6b.pdf", "Gr 8 Breuke Fractions (3) 8.1.7.pdf", "Gr 8 Breuke Fractions (4) 8.1.8.pdf", "Gr 8 Breuke Fractions (5) 8.1.9.pdf", "Gr 8 Finansies Finance (1) 8.1.3 (1).pdf", "Gr 8 Finansies Finance (2) 8.1.4.pdf", "Gr 8 Koers Rate 8.1.3b.pdf", "Gr 8 Telgetalle Whole nr (2) 8.1.2.pdf", "Gr 8 Telgetalle Whole nrs (2) 8.1.2.pdf", "Gr 8 Verhoudings ratios 8.1.3.pdf", "Gr 8.1.1 Telgetalle Whole numbers (1) (1).pdf"], "Term 2": ["Gr 8 Eksponente exponents (1) 8.2.1.pdf", "Gr 8 Eksponente exponents (2) 8.2.2.pdf", "Gr 8 Eksponente exponents (3) 8.2.3.pdf", "Gr 8 Eksponente Exponents (4) 8.2.4.pdf", "Gr 8 Eksponente Exponents (5) 8.2.5.pdf", "Gr 8 Getalpatrone Nr patterns (1) 8.2.6.pdf", "Note 14 Aug 2023.pdf"], "Term 3": ["Gr 8 Algebra (1).pdf", "Gr 8 Algebra Bundle Kw 3 (1).pdf", "Gr 8 Meetkunde 1 2024-07-19 12_58_23 (2).pdf", "Gr 8 Meetkunde 2 Copy (1).pdf", "Gr 8Algebra (2) 8.3.2.pdf", "Parallel_Lines_A3.pdf"], "Term 4": ["Gr 8 Algebra (1).pdf", "Gr 8 Algebra Bundle Kw 3 (1).pdf", "Gr 8 Breuke Fractions (1) 8.1.5.pdf", "Gr 8 Breuke Fractions (1) 8.1.5b.pdf", "Gr 8 Breuke Fractions (2) 8.1.6 (1).pdf", "Gr 8 Breuke Fractions (2) 8.1.6b.pdf", "Gr 8 Breuke Fractions (3) 8.1.7.pdf", "Gr 8 Breuke Fractions (4) 8.1.8.pdf", "Gr 8 Breuke Fractions (5) 8.1.9.pdf", "Gr 8 Eksponente exponents (1) 8.2.1.pdf", "Gr 8 Eksponente exponents (2) 8.2.2.pdf", "Gr 8 Eksponente exponents (3) 8.2.3.pdf", "Gr 8 Eksponente Exponents (4) 8.2.4.pdf", "Gr 8 Eksponente Exponents (5) 8.2.5.pdf", "Gr 8 Finansies Finance (1) 8.1.3 (1).pdf", "Gr 8 Finansies Finance (2) 8.1.4.pdf", "Gr 8 Getalpatrone Nr patterns (1) 8.2.6.pdf", "Gr 8 Koers Rate 8.1.3b.pdf", "Gr 8 Meetkunde 1 2024-07-19 12_58_23 (2).pdf", "Gr 8 Meetkunde 2 Copy (1).pdf", "Gr 8 Omtr Opp Per Area (2)8.3.8 (1).pdf", "Gr 8 Pythagoras 8.3.9 (1).pdf", "Gr 8 Telgetalle Whole nr (2) 8.1.2.pdf", "Gr 8 Telgetalle Whole nrs (2) 8.1.2.pdf", "Gr 8 Verhoudings ratios 8.1.3.pdf", "Gr 8.1.1 Telgetalle Whole numbers (1) (1).pdf", "Gr 8Algebra (2) 8.3.2.pdf", "Note 14 Aug 2023.pdf"]}, "Test Papers": {"Term 1": 0, "Term 2": 0, "Term 3": ["Graad 8_Wisk (2) (2).pdf"], "Term 4": ["2015 ANA Graad 8 Wiskunde Toets (2).pdf", "Gr8_Nov_Wiskunde_eksamenvraestel_Marise_Oberholzer (1) (1).pdf", "Gr8_Nov_Wiskunde_eksamenvraestel_Marise_Oberholzer (1) (2).pdf", "Math P1 2018 November Memo_hlayiso.com_.pdf", "Math P1 2018 November_hlayiso.com_.pdf", "Math P1 2019 November_hlayiso.com_-1.pdf", "Math P1 2019 November_hlayiso.com_.pdf", "November_Exam_2015_Grade_8 (1).pdf"]}, "Worksheets": {"Term 1": 0, "Term 2": ["Numeriese en meetkundige patrone.pdf", "worksheet_8_-_algebraic_expressions_term_2 (5).pdf", "worksheet_9_-_algebraic_equations_term_2 (5).pdf"], "Term 3": ["2D Vorms.pdf", "Driehoeke.pdf", "Kongruensie en gelykvormigheid (2).pdf", "Lyne en hoeke.pdf", "Meetkunde - Vierhoeke.pdf", "Meetkunde van 2D-vorms.pdf", "Worksheet-12-Geometry-of-straight-lines (1).pdf", "worksheet_11_-_Geometry_of_2D_shapes_grade_8_term (1).pdf"], "Term 4": ["Gr 8 - Kwartaal 4 - Wiskunde hersiening.pdf", "Grade 8 - Mathmatics.pdf", "Oppervlakte en omtrek (1).pdf", "Oppervlakte en Omtrek van 2D-Vorms.pdf", "Sirkels.pdf", "Stelling van Pythagoras.pdf", "worksheet-15-Pythagoras (1).pdf", "Worksheet-16-Area-and-Perimeter-of-2D-shapes.pdf"]}}, "Grade 10": 0}}'
 let files = JSON.parse(jsonString)
@@ -8,11 +9,13 @@ function header_div(){
     const sitenameheader_div = document.createElement("div");
     sitenameheader_div.style = ""
     sitenameheader_div.id = "siteNameHeaderDivID";
-    body.appendChild(sitenameheader_div)
+    sitenameheader_div.style.position = "relative";
+    sitenameheader_div.style.zIndex = "2";
+    body.appendChild(sitenameheader_div);
 
     const sitenameheader_title = document.createElement("h1");
     sitenameheader_title.id = "siteNameHeaderTitleID";
-    sitenameheader_title.textContent = "Mr Uys Maths";
+    sitenameheader_title.textContent = "MathHub";
     const line = document.createElement("hr")
     sitenameheader_div.appendChild(sitenameheader_title);
     sitenameheader_div.appendChild(line) 
@@ -21,6 +24,8 @@ function header_div(){
 function language_div(){
     const language_div = document.createElement("div");
     language_div.id = "languageDivID";
+    language_div.style.position = "relative";
+    language_div.style.zIndex = "2";
     body.appendChild(language_div);
 
     const language_form = document.createElement("form");
@@ -67,22 +72,34 @@ function chooseLanguage(language){
         }
     });
     if (language == "Afrikaans"){
-        // Title
-        titletext.textContent = "Mnr Uys Wiskunde"
-
         // Main Page Visibility
         mainPageAfrikaans.style = "display: block"
+        mainPageAfrikaans.style.zIndex = "2"
+        mainPageAfrikaans.style.position = "relative"
         mainPageEnglish.style = "display: none"
 
     }
     else if (language == "English"){
-        // Title
-        titletext.textContent = "Mr Uys Maths"
-
         // Main Page visibility
         mainPageAfrikaans.style = "display: none"
         mainPageEnglish.style = "display: block"
+        mainPageEnglish.style.position = "relative"
+        mainPageEnglish.style.zIndex = "2"
     };
+}
+function image_div(){
+    const imageDiv = document.createElement("div");
+    imageDiv.id = "ImageDiv"
+    //imageDiv.style.position = "absolute"
+    //imageDiv.style.backgroundSize = "cover"
+    //imageDiv.style.top = "50%"
+    //imageDiv.style.left = "50%"
+    imageDiv.style.backgroundImage = 'url("Background MathHub.jpg")'
+    //imageDiv.style.zIndex = "1"
+    //imageDiv.style.height = "40em"
+    //imageDiv.style.width = "40em"
+    //imageDiv.style.transform = "translate(-50%, -50%)"
+    body.appendChild(imageDiv)
 }
 
 function create_main_divs(){
@@ -91,6 +108,8 @@ function create_main_divs(){
     for(let i = 0; i < languages.length; i++){
         const buttonList = Object.keys(files[languages[i]])
         const div = document.createElement("div")
+        div.style.position = "relative";
+        div.style.zIndex = "2";
         div.id = languages[i] + "Div"
         if (languages[i] == "Afrikaans"){
             div.style = "display: none";
@@ -105,6 +124,7 @@ function create_main_divs(){
             const identity = languages[i] + buttonList[a] + "Div"
             button.onclick = function(){
                 div.style.display = "none";
+                
                 document.getElementById("languageDivID").style.display = "none"
                 document.getElementById(identity).style.display = "block";
             }
@@ -124,6 +144,8 @@ function create_level_1_divs(lang){
     for (let i = 0; i < folders_list.length; i++){
         const div = document.createElement("div");
         div.style.display = "none"
+        div.style.position = "relative";
+        div.style.zIndex = "2";
         const identity = lang + folders_list[i] + "Div"
         div.id = identity;
 
@@ -172,6 +194,8 @@ function create_level_2_divs(lang, level_1){
     for (let i = 0; i < folders_list.length; i++){
         const div = document.createElement("div");
         div.style.display = "none"
+        div.style.position = "relative";
+        div.style.zIndex = "2";
         const identity = lang + level_1 + folders_list[i] + "Div"
         div.id = identity;
 
@@ -219,6 +243,8 @@ function create_level_3_divs(lang, level_1, level_2){
     for (let i = 0; i < folders_list.length; i++){
         const div = document.createElement("div");
         div.style.display = "none"
+        div.style.position = "relative";
+        div.style.zIndex = "2";
         const identity = lang + level_1 + level_2 + folders_list[i] + "Div"
         div.id = identity;
 
@@ -264,9 +290,13 @@ function create_level_3_divs(lang, level_1, level_2){
 
 
 function main(){
+    
     header_div();
     language_div();
     create_main_divs();
+    image_div();
+    //window.onload = image_div;
+    
     //alert(Object.keys(files["Afrikaans"]))
 }
 
